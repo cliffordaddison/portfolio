0a9a738c572fc7314850e5c573aeb24b307885c2
Clifford Addison <35921339+cliffordaddison@users.noreply.github.com>
2025-06-24 17:57:30 -0400

    Update files in src

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Home from './pages/index';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './pages/index'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
-);
+)
